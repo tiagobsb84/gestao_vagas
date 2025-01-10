3c1c73daac1824fca4f84b0e38677567252aef3f
Tiago Silva <tiagobsb84@gmail.com>
2025-01-10 17:09:58 -0300

    Adicionando a estrutura do projeto e as dependencias, spring web, lombok, devtools, validation.

---
+++ src/test/java/br/com/tiago/vagas/VagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiago.vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
